0368dacdc61a991374b047b31ed7781618332e13
parkie <69488850+mlodyskiny@users.noreply.github.com>
2025-01-03 14:47:25 +0100

    Proper bot names, 1:1 replication. Fix S20+. Remove hooking.

--- Project Reboot 3.0/botnames.h
+++ Project Reboot 3.0/botnames.h
@@ -11,62 +11,107 @@ inline std::vector<FString> PlayerBotNames;
 
 static inline void InitBotNames()
 {
-    PlayerBotNames.clear();
-
-    PlayerBotNames.push_back(L"qwertyouriop");
-    PlayerBotNames.push_back(L"willdey");
-    PlayerBotNames.push_back(L"Soggs");
-    PlayerBotNames.push_back(L"vxzty");
-    PlayerBotNames.push_back(L"Milxnor");
-    PlayerBotNames.push_back(L"max");
-    PlayerBotNames.push_back(L"Callum");
-    PlayerBotNames.push_back(L"Samicc");
-    PlayerBotNames.push_back(L"AidasP");
-    PlayerBotNames.push_back(L"danii");
-    PlayerBotNames.push_back(L"sizzy");
-    PlayerBotNames.push_back(L"penguin");
-    PlayerBotNames.push_back(L"Jagger");
-    PlayerBotNames.push_back(L"Jacobb");
-    PlayerBotNames.push_back(L"Zulu");
-    PlayerBotNames.push_back(L"kemo");
-    PlayerBotNames.push_back(L"Ender");
-    PlayerBotNames.push_back(L"karma");
-    PlayerBotNames.push_back(L"Samuel");
-    PlayerBotNames.push_back(L"Kyiro");
-    PlayerBotNames.push_back(L"Ahava");
-    PlayerBotNames.push_back(L"Spooky");
-    PlayerBotNames.push_back(L"Akos");
-    PlayerBotNames.push_back(L"ridecly");
-    PlayerBotNames.push_back(L"cardurr");
-    PlayerBotNames.push_back(L"android");
-    PlayerBotNames.push_back(L"Sync");
-    PlayerBotNames.push_back(L"GD");
-    PlayerBotNames.push_back(L"Jeremy");
-    PlayerBotNames.push_back(L"TeoVR");
-    PlayerBotNames.push_back(L"CuteLess");
-    PlayerBotNames.push_back(L"Trash Bot");
-    PlayerBotNames.push_back(L"Lawin");
-    PlayerBotNames.push_back(L"Adam");
-    PlayerBotNames.push_back(L"Ruby");
-    PlayerBotNames.push_back(L"Rythm");
-    PlayerBotNames.push_back(L"Fexor");
-    PlayerBotNames.push_back(L"Windermed");
-    PlayerBotNames.push_back(L"BIGGINS");
-    PlayerBotNames.push_back(L"Charles");
-    PlayerBotNames.push_back(L"Custox");
-    PlayerBotNames.push_back(L"Kaede");
-    PlayerBotNames.push_back(L"lintu");
-    PlayerBotNames.push_back(L"Baby");
-    PlayerBotNames.push_back(L"Mineluke");
-    PlayerBotNames.push_back(L"wiktorwiktor12");
-    PlayerBotNames.push_back(L"solo");
-    PlayerBotNames.push_back(L"HxD");
-    PlayerBotNames.push_back(L"Noggo");
-    PlayerBotNames.push_back(L"weyn");
-    PlayerBotNames.push_back(L"ralz");
-    PlayerBotNames.push_back(L"farex");
-    PlayerBotNames.push_back(L"Nam");
-    PlayerBotNames.push_back(L"AllyJax");
-    PlayerBotNames.push_back(L"secret_pommes");
-    PlayerBotNames.push_back(L"Twin1");
+    PlayerBotNames =
+    {
+        L"[EPIC] Tim Sweeney",
+        L"[EPIC] MagmaReef",
+        L"[Rift] Makks",
+        L"[Era] SizzyFNBRLeaks",
+        L"[EOR] Nyaai",
+        L"[Chronos] parkie",
+        L"[1crAp] IdystFatass",
+        L"Samicc/FortniteBuilds",
+        L"KyiroEasyInstaller",
+        L"[Nova] Ender0001",
+        L"[OnyxFN] Jacobb626YT",
+        L"SLL Bypass Patched",
+        L"[Nova] Sammy",
+        L"[Beyond] Twin2",
+        L"[Trannies] Ploosh",
+        L"[Trannies] Skies",
+        L"[Trannies] Jagger",
+        L"[Femboys] Forty",
+        L"[Sonic Game] array0x",
+        L"[FMod Linora] Android1337",
+        L"[Greek] Skidaim",
+        L"[EZFN] Lupus joel",
+        L"[LawinServer] Lawin",
+        L"TeamKillerOzne",
+        L"[Fatasses] Soggs",
+        L"[Polaris V2] Cyuubi",
+        L"[Dumper-7] Fischsalat",
+        L"[Retrac] Zombie Man 16k Tokens",
+        L"[Retrac] 0xKAEDE KSKID",
+        L"[Retrac] Blur",
+        L"GDBOI101",
+        L"FortPickle",
+        L"FortMP is era not twin1",
+        L"[Nova] Kyiro sakcnana",
+        L"Fmod wasnt tokenlogger",
+        L"Googlecheats karma",
+        L"[REtard] 1shotfishy",
+        L"[REtard] Marvelco",
+        L"[Twin1] Twin towers",
+        L"Dyno [ BOT \u2705 ]",
+        L"[JEWS] Riko",
+        L"[REtard PEDO] spooky",
+        L"[REtard ezfn] anto",
+        L"[REtard ezfn] represoh",
+        L"[REtard] Timmie Doesnt develop",
+        L"[Project Reboot] Milf nor",
+        L"[Fatass jew] Tiva",
+        L"[Cheater] Custox",
+        L"[greeKS] Wiks wiks",
+        L"ShieldMP",
+        L"[Fat] Biggins",
+        L"[Aurora V2] Brianna",
+        L"PeQuFNBRLeaks",
+        L"[ChannelMP] ZETAX",
+        L"Huggo",
+        L"[SizzyFNBRLeaks] Era",
+        L"Text channel #general",
+        L"Radmin vpn",
+        L"Logmein hamachi riftmp",
+        L"Zerotier I FUCKING HATE YOU",
+        L"[Fat] Threxo",
+        L"[WGS] WiktorWiktor12 Bri ish",
+        L"[Reboot UltimateSkid] Max",
+        L"[Skidduh] Magmadev",
+        L"[Makks] RiftMp",
+        L"[Beat] Aurora.Runtime",
+        L"[Cheatuhhhh] Random",
+        L"[UePak] Zylox",
+        L"[RetARD] Sali",
+        L"[RetARD] Grapz",
+        L"Fortnite.Tools 13.40 StormFn S13 full games",
+        L"5 Minute no kick",
+        L"[Hosting] Epic",
+        L"[Sulfur top 1 skid] Jaysen",
+        L"[FAREX] Fmof",
+        L"[Idfk] Noggo",
+        L"McMistrzYt",
+        L"[StormFN S13] Stormzy",
+        L"FaZe Cinardere",
+        L"[Eon] Charles",
+        L"[Turkey] Hxnted",
+        L"[Yt view farmer] Mineluke",
+        L"[Rythm] Ryhtm",
+        L"SwAyZz",
+        L"Eon owner rio tokenlog",
+        L"[FLIPPED] Not tacs",
+        L"Flipped minus 2k members epic nuke by parkie",
+        L"ElixirFN",
+        L"BetterFN",
+        L"[Flopper] Mstreem",
+        L"[Trannies] Zinx",
+        L"Default idk what he did no name",
+        L"[Trannies] Neva",
+        L"[SKIDZ] burlone SKId",
+        L"[LowTaperFade] Ninja",
+        L"[4.5] Crash log",
+        L"EOR.dll",
+        L"[Manora] MaTiD",
+        L"[harmony]JYZO",
+        L"[Chikcen] Miles"
+    };
 }
